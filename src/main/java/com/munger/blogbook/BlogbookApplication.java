9a55a413a1fe4cf17bb40b2d6d69b3457cf73ad5
13_liqingsong <wang19yi17@163.com>
2019-07-05 20:17:24 +0800

    建立项目--2019年7月5日 20:15:17

---
+++ src/main/java/com/munger/blogbook/BlogbookApplication.java
@@ -0,0 +1,13 @@
+package com.munger.blogbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/munger/blogbook/BlogbookApplicationTests.java
@@ -0,0 +1,16 @@
+package com.munger.blogbook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlogbookApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
